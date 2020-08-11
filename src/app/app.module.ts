07ce6c94be340c23a68cd5e490f2de5e82a0eb30
Abdulaziz Ladan <abdulazizladan@gmail.com>
2020-08-11 16:49:58 +0100

    task: Init Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { VrComponent } from './dashboard/vr/vr.component';
+import { CbtComponent } from './dashboard/cbt/cbt.component';
+import { QuestionComponent } from './dashboard/cbt/question/question.component';
+import { LibraryComponent } from './dashboard/library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AdminComponent,
+    VrComponent,
+    CbtComponent,
+    QuestionComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/cbt/cbt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cbt',
+  templateUrl: './cbt.component.html',
+  styleUrls: ['./cbt.component.scss']
+})
+export class CbtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
